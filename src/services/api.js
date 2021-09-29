475eb289f0578c366b8daea82f2af4603e4075e9
Akan Asanga <akanasanga@outlook.com>
2021-09-30 00:41:39 +0100

    stage

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = /* 'http://localhost:3000/' */ 'https://appointment202.herokuapp.com/'
+axios.defaults.baseURL = 'https://appointment202.herokuapp.com/' /* 'http://localhost:3000/' */
 
 axios.defaults.headers.common.Authorization =
     'Bearer ' + localStorage.getItem('token')
