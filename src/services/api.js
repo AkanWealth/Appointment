f42b37cebf7a8e2e4ec6bda90cc61038fd635c7b
Akan Asanga <akanasanga@outlook.com>
2021-09-30 08:14:36 +0100

    fixing cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'https://appointment202.herokuapp.com/' /* 'http://localhost:3000/' */
-//set access control allow origin to allow cross origin requests
+// set access control allow origin to allow cross origin requests
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://appointment202.herokuapp.com/'
 axios.defaults.headers.common.Authorization =
