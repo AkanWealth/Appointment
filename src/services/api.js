fd96a1e82da1558488f605e5fe2c5bcab201d622
Akan Asanga <akanasanga@outlook.com>
2021-09-30 08:56:43 +0100

    checking

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,11 @@
 import axios from 'axios'
-// import authorization from '../../../server/middlewares/authorization'
 
 // axios.defaults.baseURL = 'https://appointment202.herokuapp.com/' /* 'http://localhost:3000/' */
 // set access control allow origin to allow cross origin requests
 axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://appointment202.herokuapp.com/'
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
-// authorization middleware for all requests to the api
-// authorization header is set to the token in the local storage
-axios.interceptors.request.use(config => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer + ${token}`
-  }
-  return config
-})
-// axios.defaults.headers.common.Authorization =
-//     'Bearer ' + localStorage.getItem('token')
+// axios.defaults.headers["content-type"] = "application/json";
+// axios.defaults.headers.common.authorization = `Bearer ${token}`;
+
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
+axios.defaults.headers.common.Authorization =
+    'Bearer ' + localStorage.getItem('token')
