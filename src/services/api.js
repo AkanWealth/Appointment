6449953dcf1669efcb30074b6a5cb022b0463b9f
Akan Asanga <akanasanga@outlook.com>
2021-09-30 08:06:12 +0100

    test for cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://appointment202.herokuapp.com/' /* 'http://localhost:3000/' */
+// axios.defaults.baseURL = 'https://appointment202.herokuapp.com/' /* 'http://localhost:3000/' */
 
+// cors proxy for heroku server to allow cross origin requests from client side to heroku server
+axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://appointment202.herokuapp.com/'
 axios.defaults.headers.common.Authorization =
     'Bearer ' + localStorage.getItem('token')
