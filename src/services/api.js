40b4817cc2ef691a6411916f4235c1a4a6c89143
Akan Asanga <akanasanga@outlook.com>
2021-09-30 08:24:06 +0100

    fixing

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 // axios.defaults.baseURL = 'https://appointment202.herokuapp.com/' /* 'http://localhost:3000/' */
 // set access control allow origin to allow cross origin requests
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
-axios.defaults.baseURL = 'https://appointment202.herokuapp.com/'
+axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://appointment202.herokuapp.com/'
 axios.defaults.headers.common.Authorization =
     'Bearer ' + localStorage.getItem('token')
