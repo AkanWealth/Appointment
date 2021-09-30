9c95d9ae243316ec251df963f4eef168c9444f8f
Akan Asanga <akanasanga@outlook.com>
2021-09-30 08:09:51 +0100

    fixing error

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'https://appointment202.herokuapp.com/' /* 'http://localhost:3000/' */
-
-// cors proxy for heroku server to allow cross origin requests from client side to heroku server
+//set access control allow origin to allow cross origin requests
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://appointment202.herokuapp.com/'
 axios.defaults.headers.common.Authorization =
     'Bearer ' + localStorage.getItem('token')
