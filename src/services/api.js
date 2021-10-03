b8a52e2b79a1234b3211dfc1121afe084b5fdf1f
Akan Asanga <akanasanga@outlook.com>
2021-10-03 15:40:39 +0100

    working

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'https://appointment202.herokuapp.com/' /* 'http://localhost:3000/' */
-// set access control allow origin to allow cross origin requests
-axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://appointment202.herokuapp.com/'
-// axios.defaults.headers["content-type"] = "application/json";
-// axios.defaults.headers.common.authorization = `Bearer ${token}`;
+// axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://appointment202.herokuapp.com/'
+axios.defaults.baseURL = 'http://localhost:3000/'
 
 axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
 axios.defaults.headers.common.Authorization =
