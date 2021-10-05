c3603fb0d6be77f6af924cb665037d4f8d502278
Akan Asanga <akanasanga@outlook.com>
2021-10-05 13:02:16 +0100

    testting

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://appointment202.herokuapp.com/'
-axios.defaults.baseURL = /* 'http://localhost:3000/' || */ 'https://blue-force.herokuapp.com/'
+axios.defaults.baseURL = 'https://blue-force.herokuapp.com/'
 
-axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
+// axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
 axios.defaults.headers.common.Authorization =
     'Bearer ' + localStorage.getItem('token')
